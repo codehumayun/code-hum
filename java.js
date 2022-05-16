d79d302ebc468cdc661461689bd9916d34e5f9ba
codehumayun <codehumayun@gmail.com>
2022-05-16 06:29:46 -0700

    Distance calculator

---
+++ java.js
@@ -0,0 +1,53 @@
+// var lahore = '32';
+// var multan = 22;
+
+// var result = parseInt(lahore) + multan;
+
+// prompt(result)
+
+
+// var lahore = 12;
+// var multan = 15;
+
+// if(lahore < multan && multan != lahore ){
+
+//     console.log('this is check it');
+// }
+
+
+// var a = 34;
+// var b = 33;
+// var city = prompt('lahore')
+
+// if(a > b){
+    
+//     alert('system Alert');
+//     confirm('Are you sure CLICK OK');
+//     prompt("Select your city");
+// }
+// else{
+
+//        ("you have no identification")
+// }
+
+  
+
+// var city = prompt('Check to your destance km Lahore to other city');
+
+// if(city.toLowerCase() == "karachi"){
+ 
+//     document.write('Karachi is (1,210.8 km) to lahore');
+
+//     if(city== "karachi"){
+
+//         document.write("14 hr 15 min via Sukkur - Multan Motorway/M-5")
+//     }
+
+
+// }else{
+
+
+// }
+
+
+
